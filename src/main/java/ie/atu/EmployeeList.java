14dcd46fc38ef329fb0ab12cece1a8ca722759d5
g00370587 <g00370587@atu.ie>
2022-12-07 10:28:31 +0000

    employee arraylist methods created

--- src/main/java/ie/atu/EmployeeList.java
+++ src/main/java/ie/atu/EmployeeList.java
@@ -1,6 +1,24 @@
 package ie.atu;
+import java.util.ArrayList;
 
 public class EmployeeList {
+
+    ArrayList<Object> Employees = new ArrayList<>();
+
+     public ArrayList<Object> addNewEmployee (Employee newEmployee) {
+        Employees.add(newEmployee) ;
+        return Employees;
+    }
+
+     public ArrayList<Object> deleteEmployee(int id) {
+       Employees.remove(id);
+       return Employees;
+    }
+
+    public void getListSize() {
+        System.out.println("the size of the arrayList is: " + Employees.size());
+    }
+
     public static void main(String[] args) {
 
     }
